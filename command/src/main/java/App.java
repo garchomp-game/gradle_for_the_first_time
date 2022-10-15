78459c5c0ec9a754c7b67f780133615a0fae0464
garchomp-game <otoka.japanesedrum@gmail.com>
2022-10-15 14:15:39 +0900

    astrovimで一部整形

--- command/src/main/java/App.java
+++ command/src/main/java/App.java
@@ -1,13 +1,13 @@
+import java.io.File;
+import java.nio.file.*;
+import java.util.*;
+import java.util.concurrent.Callable;
+import myEnum.PathName;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
-import java.io.File;
-import java.nio.file.*;
-import java.util.concurrent.Callable;
-import java.util.*;
 import service.FileManager;
-import myEnum.PathName;
 /*
 例えばpath_configに
 lang=gcc // node ruby java...
@@ -15,44 +15,37 @@ default_mode=full // compile run...
 default_code_path=path/to/
 default_source_path=path/to/
 */
-@Command(
-  name = "run", 
-  mixinStandardHelpOptions = true, 
-  version = "1.0",
-  description = "様々な言語のコンパイルと実行をしやすくしたコマンド"
-)
+@Command(name = "run", mixinStandardHelpOptions = true, version = "1.0",
+         description = "様々な言語のコンパイルと実行をしやすくしたコマンド")
 public class App implements Callable<Integer> {
-  @Option(names = {"-l", "--lang"},
-  defaultValue = "",
-  description = "言語のセットができます")
+  @Option(names = {"-l", "--lang"}, defaultValue = "",
+          description = "言語のセットができます")
   private String lang;
-  
-  @Option(names = {"-c", "--compile"},
-  description = "コンパイルをします")
+
+  @Option(names = {"-c", "--compile"}, description = "コンパイルをします")
   private boolean compile;
 
   @Option(names = {"-e", "--execution"},
-  description = "コンパイルしたファイルを実行します")
+          description = "コンパイルしたファイルを実行します")
   private boolean execution;
-  
-  @Option(names = {"-f", "--full"},
-  description = "フル実行")
+
+  @Option(names = {"-f", "--full"}, description = "フル実行")
   private boolean full;
-  
-  @Parameters(paramLabel = "FILE", description = "ファイル取得")
-  File[] files;
-  
-  @Option(names = {"-h", "--help"}, usageHelp = true, description = "ヘルプ一覧")
+
+  @Parameters(paramLabel = "FILE", description = "ファイル取得") File[] files;
+
+  @Option(names = {"-h", "--help"}, usageHelp = true,
+          description = "ヘルプ一覧")
   private boolean helpRequested = false;
-  
+
   @Override
   public Integer call() throws Exception {
     System.out.println(this.lang);
     Path[] pathList = new Path[PathName.PATHLENGTH.get()];
-    if(Objects.isNull(this.files))
-      throw new NullPointerException("ファイルを指定してください");  
+    if (Objects.isNull(this.files))
+      throw new NullPointerException("ファイルを指定してください");
     else
-      for(int i = 0; i < files.length; i++)
+      for (int i = 0; i < files.length; i++)
         pathList[i] = this.files[i].toPath();
     FileManager fm = new FileManager(this.files, pathList);
     fm.setLang(this.lang);
--- command/src/main/java/service/MyBuilder.java
+++ command/src/main/java/service/MyBuilder.java
@@ -1,38 +1,78 @@
 package service;
-import java.nio.file.*;
-import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+
+import logic.FileLogic;
 import myEnum.Languages;
 import myEnum.PathName;
-import java.io.*;
-import org.apache.commons.io.FilenameUtils;
 import myException.NotMatchLangException;
-import logic.FileLogic;
 
 public class MyBuilder {
+  public Path[] getPathList() {
+    return pathList;
+  }
+
+  public void setPathList(final Path[] pathList) {
+    this.pathList = pathList;
+  }
+
+  public String getLang() {
+    return lang;
+  }
+
+  public void setLang(final String lang) {
+    this.lang = lang;
+  }
+
+  public String getExtension() {
+    return extension;
+  }
+
+  public String getFileName() {
+    return fileName;
+  }
+
+  public String getBaseName() {
+    return baseName;
+  }
+
+  public FileLogic getFileLogic() {
+    return fileLogic;
+  }
+
+  public void setFileLogic(final FileLogic fileLogic) {
+    this.fileLogic = fileLogic;
+  }
+
   private Path[] pathList = new Path[PathName.PATHLENGTH.get()];
   private String lang;
   private String extension;
   private String fileName;
   private String baseName;
   FileLogic fileLogic;
-  public MyBuilder(Path[] pathList, String lang) {
+
+  public MyBuilder(final Path[] pathList, final String lang) {
     this.pathList = pathList;
     this.lang = lang;
     this.fileLogic = new FileLogic();
   }
-  
-  public void setFileName(String fileName) {
+
+  public void setFileName(final String fileName) {
     this.fileName = fileName;
   }
-  public void setExtension(String extension) {
+
+  public void setExtension(final String extension) {
     this.extension = extension;
   }
-  public void setBaseName(String baseName) {
+
+  public void setBaseName(final String baseName) {
     this.baseName = baseName;
   }
-  
-  public Languages build(Languages language) {
-    switch(language) {
+
+  public Languages build(final Languages language) {
+    switch (language) {
       case CLANG:
         return this.clang();
       case JAVA:
@@ -42,86 +82,86 @@ public class MyBuilder {
     }
     return null;
   }
-  
+
   private Languages clang() {
-    if(!this.extension.equals("c")) {
+    if (!this.extension.equals("c")) {
       try {
         throw new NotMatchLangException("C言語");
-      } catch (NotMatchLangException e) {
+      } catch (final NotMatchLangException e) {
         e.printStackTrace();
         System.exit(0);
       }
     }
-    StringBuilder command = new StringBuilder();
+    final StringBuilder command = new StringBuilder();
     command.append("gcc ");
     command.append(this.fileName);
-    if(this.fileLogic.isMoveDirectory(this.pathList)) {
+    if (this.fileLogic.isMoveDirectory(this.pathList)) {
       command.append(" -o ");
       command.append(this.baseName);
     }
-    if(build(command.toString()))
+    if (build(command.toString()))
       return Languages.CLANG;
     return null;
   }
-  
+
   private Languages java() {
     System.out.println("java");
-    if(!this.extension.equals("java")) {
+    if (!this.extension.equals("java")) {
       try {
         throw new NotMatchLangException("java");
-      } catch(NotMatchLangException e) {
+      } catch (final NotMatchLangException e) {
         e.printStackTrace();
         System.exit(0);
       }
     }
-    StringBuilder command = new StringBuilder();
+    final StringBuilder command = new StringBuilder();
     command.append("javac ");
-    if(this.fileLogic.isMoveDirectory(this.pathList)) {
+    if (this.fileLogic.isMoveDirectory(this.pathList)) {
       try {
         command.append("-d ");
         command.append(this.pathList[PathName.DIRECTORYNAME.get()].toString());
-      } catch (Exception e) {
+      } catch (final Exception e) {
         e.printStackTrace();
         System.exit(0);
       }
     }
     command.append(this.fileName);
-    if(build(command.toString()))
+    if (build(command.toString()))
       return Languages.JAVA;
     return null;
   }
-  
+
   private Languages node() {
-    if(!this.extension.equals("js")) {
+    if (!this.extension.equals("js")) {
       try {
         throw new NotMatchLangException("js");
-      } catch(NotMatchLangException e) {
+      } catch (final NotMatchLangException e) {
         e.printStackTrace();
         System.exit(0);
       }
     }
-    String command = "node " + this.fileName;
-    if(build(command))
+    final String command = "node " + this.fileName;
+    if (build(command))
       return Languages.NODE;
     return null;
   }
-  
-  private boolean build(String command) {
+
+  private boolean build(final String command) {
     try {
-      Process process = Runtime.getRuntime().exec(command);
-      BufferedReader reader = new BufferedReader(
-        new InputStreamReader(process.getInputStream()));
-      
-      StringBuilder output = new StringBuilder();
+      final Process process = Runtime.getRuntime().exec(command);
+      final BufferedReader reader = new BufferedReader(
+          new InputStreamReader(process.getInputStream()));
+
+      final StringBuilder output = new StringBuilder();
       String line;
-      String br = System.getProperty("line.separator");
+      final String br = System.getProperty("line.separator");
       while ((line = reader.readLine()) != null) {
         output.append(line);
         output.append(br);
       }
       System.out.println(output.toString());
       return true;
-    } catch (Exception e) {
+    } catch (final Exception e) {
       e.printStackTrace();
     }
     return false;
